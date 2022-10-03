8ee480fc038c936058bb60291f92d0aa0b3b7b9a
Caian marcinkowski <caianferre@gmail.com>
2022-10-03 19:22:22 -0300

    First commit

---
+++ app/src/main/java/com/portfolio/geolocation_java/Geolocation.java
@@ -0,0 +1,53 @@
+package com.portfolio.geolocation_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class Geolocation extends AppCompatActivity {
+      
+      @Override
+      protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            getSupportActionBar().hide();
+            setContentView(R.layout.activity_geolocation);
+      }
+      
+      public void SearchGeolocation(View v) {
+            
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                  
+                  ActivityCompat.requestPermissions(Geolocation.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+                  ActivityCompat.requestPermissions(Geolocation.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+                  ActivityCompat.requestPermissions(Geolocation.this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE}, 1);
+                  
+                  return;
+            }
+      
+            LocationManager locationManager = (LocationManager) getSystemService(Geolocation.this.LOCATION_SERVICE);
+            LocationListener locationListener = new LocationListener();
+            
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            
+            if(locationManager.isProviderEnabled(locationManager.GPS_PROVIDER)){
+                  String message = "Latitude: " + locationListener.getLatitude() + "/n" +
+                          "Longitude: " + locationListener.getLongitude();
+      
+                  Toast.makeText(Geolocation.this, message, Toast.LENGTH_LONG);
+            } else {
+                  Toast.makeText(Geolocation.this, "GPS not enabled!", Toast.LENGTH_LONG);
+            }
+      
+            WebView webView = findViewById(R.id.wvMap);
+            webView.getSettings().setJavaScriptEnabled(true);
+            webView.loadUrl("https://www.google.com/maps/search/?api=1&query="+locationListener.getLatitude()+","+locationListener.getLongitude());
+      }
+}
---
+++ app/src/main/java/com/portfolio/geolocation_java/LocationListener.java
@@ -0,0 +1,41 @@
+package com.portfolio.geolocation_java;
+
+import android.location.Location;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+
+public class LocationListener implements android.location.LocationListener {
+      
+      private static double latitude, longitude;
+      
+      @Override
+      public void onLocationChanged(@NonNull Location location) {
+            this.latitude = location.getLatitude();
+            this.longitude = location.getLongitude();
+      }
+      
+      public static double getLatitude() {
+            return latitude;
+      }
+      
+      public static double getLongitude() {
+            return longitude;
+      }
+      
+      @Override
+      public void onStatusChanged(String provider, int status, Bundle extras) {
+            android.location.LocationListener.super.onStatusChanged(provider, status, extras);
+      }
+      
+      @Override
+      public void onProviderEnabled(@NonNull String provider) {
+            android.location.LocationListener.super.onProviderEnabled(provider);
+      }
+      
+      @Override
+      public void onProviderDisabled(@NonNull String provider) {
+            android.location.LocationListener.super.onProviderDisabled(provider);
+      }
+}
